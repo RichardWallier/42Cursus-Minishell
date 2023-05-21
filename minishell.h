3e5e831cdb73c351a8cb39d030d1f60bd8beaebc
Lguedes <lohan.guedes@nutrix.tech>
2023-05-21 01:26:55 -0300

    Adjusting the stdio.h to be the first include

--- minishell.h
+++ minishell.h
@@ -13,10 +13,10 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+#include <stdio.h>
 #include "./libft/libft.h"
 #include <limits.h>
 #include <readline/readline.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
